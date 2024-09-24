97a59a29e180639c764cf7a3fbd4910da22f7cb4
Deez Artt <dzakyfadhilahrahmawan@gmail.com>
2024-09-24 19:40:00 +0700

    Delete soal 4 .py

--- soal 4 .py
+++
@@ -1,9 +0,0 @@
-nama_barang = 'kertas'
-jumlah_barang = 20
-harga_barang = 1000000
-ketersediaan = 'true'
-
-print ('nama barang:',nama_barang)
-print ('jumlah barang: ' ,jumlah_barang)
-print ('harga barang:',harga_barang)
-print ('ketersediaan:',ketersediaan)
