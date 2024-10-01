7ac85609b5bf8bc782f161ce7830e31701613051
Deez Artt <dzakyfadhilahrahmawan@gmail.com>
2024-10-01 21:21:36 +0700

    python file

---
+++ rumus geometri bola.py
@@ -0,0 +1,11 @@
+print ("="*40)
+print ("PROGRAM LINGKARAN ")
+print ("="*40)
+
+jari_jari = float(input("masukkan Jari-jari")) 
+
+luas = 3.14 * jari_jari
+print ("luas:", luas ," cm2")
+
+keliling = 3.14 * 2 * jari_jari
+print ("keliling:", keliling , " cm")
