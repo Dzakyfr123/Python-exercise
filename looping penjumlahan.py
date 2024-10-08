05ec023f03f3a6112ab5a4e378d8679b56f1af7d
Deez Artt <dzakyfadhilahrahmawan@gmail.com>
2024-10-08 09:01:47 +0700

    Delete looping penjumlahan.py

--- looping penjumlahan.py
+++
@@ -1,8 +0,0 @@
-total = 0
-n = int(input("Masukkan jumlah angka yang ingin dijumlahkan: "))
-
-for i in range(n):
-    angka = float(input(f"Masukkan angka ke-{i + 1}: "))
-    total += angka
-
-print(f"Total dari angka yang dimasukkan adalah: {total}")
