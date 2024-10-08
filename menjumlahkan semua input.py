4b4041e641bb108f5348bfc808c9f56f51d6648b
Deez Artt <dzakyfadhilahrahmawan@gmail.com>
2024-10-08 18:53:27 +0700

    Delete menjumlahkan semua input.py

--- menjumlahkan semua input.py
+++
@@ -1,8 +0,0 @@
-barang1 = float(input('berapa barang pertama '))
-barang2 = float(input('berapa barang kedua '))
-barang3 = float(input('berapa barang ketiga '))
-barang4 = float(input('berapa barang keempat'))
-
-jumlah_barang = barang1 + barang2 + barang3 + barang4
-
-print ('jumlah total barang:',jumlah_barang)
